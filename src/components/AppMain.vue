7b693c258016ba65de9daa5466c2727d87f1dabf
raffaelepiscitelli <raffaelepiscitelli1994@gmail.com>
2023-12-22 10:25:10 +0100

    Aggiunta stampa in  pagina

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,17 @@
 <template lang="">
     <div>
-        <h1>{{movie.name}}</h1>
+        <article v-for='movie in store.moviesArray'>
+            <p>Titolo: {{movie.title}}</p>
+            <p>Titolo originale: {{movie.original_title}}</p>
+            <p>Lingua originale: {{movie.original_language}}</p>
+            <p>Voto: {{movie.vote_average}}</p>
+        </article>
+        <article v-for='tvSeries in store.tvSeriesArray'>
+            <p>Titolo: {{tvSeries.name}}</p>
+            <p>Titolo originale: {{tvSeries.original_name}}</p>
+            <p>Lingua originale: {{tvSeries.original_language}}</p>
+            <p>Voto: {{tvSeries.vote_average}}</p>
+        </article>
     </div>
 </template>
 <script>
--- src/js/store.js
+++ src/js/store.js
@@ -3,7 +3,9 @@ import { reactive } from "vue";
 import axios from 'axios';
 export const store = reactive({
     moviesArray: [],
+    tvSeriesArray: [],
     moviesApi: "https://api.themoviedb.org/3/search/movie?api_key=a9971208a6fba9c97e3d228c3f1eac78&query=",
+    tvSeriesApi: "https://api.themoviedb.org/3/search/tv?api_key=a9971208a6fba9c97e3d228c3f1eac78&query=",
     searchMovies(searchContent = "") {
         axios.get(this.moviesApi + searchContent)
             .then((response) => {
@@ -15,5 +17,18 @@ export const store = reactive({
                 // handle error
                 console.log(error);
             })
-    }
-});
+    },
+    searchTvSeries(searchContent = "") {
+        axios.get(this.tvSeriesApi + searchContent)
+            .then((response) => {
+                // handle success
+                console.log(response.data.results);
+                this.tvSeries = response.data.results;
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+        }
+
+    });
